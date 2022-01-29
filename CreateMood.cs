369a38ec428d0fe82505c5b9a0469b0ce6d26da4
Janu shankar <janakiharishankar24@gmail.com>
2022-01-29 22:41:16 +0530

    sub program

---
+++ CreateMood.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MoodAnalyserReflection
+{
+    internal class CreateMood
+
+    {
+        string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
